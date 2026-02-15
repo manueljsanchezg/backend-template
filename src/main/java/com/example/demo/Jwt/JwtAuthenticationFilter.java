6c8cac3e76035c21ad1ec6543344ff97ace85895
manueljsanchezg <manueljsanchez10@gmail.com>
2026-02-15 01:41:21 +0100

    feat: added path auth pass

--- src/main/java/com/example/demo/Jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/Jwt/JwtAuthenticationFilter.java
@@ -28,6 +28,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        
+        String path = request.getServletPath();
+        if (path.startsWith("/api/auth/")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        
         try {
             String jwt = getJwtFromHeaders(request);
             if (jwt != null && jwtService.verifyToken(jwt)) {
